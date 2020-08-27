e7419eabf472a48372b8b04e935b1607e553df3e
Irivahs <mdshaviri786@gmail.com>
2020-08-27 18:08:30 +0530

    commit for heroku deploy

--- shavWorld/backend/server.js
+++ shavWorld/backend/server.js
@@ -34,4 +34,6 @@ app.get("/api/products/:id", (req, res) => {
 app.get("/api/products", (req, res) => {
    res.send(data.products);
 });*/
-app.listen(5000, () => {console.log("Server started at http://localhost:5000")});
+app.listen(config.PORT,
+    () => {console.log("Server started at http://localhost:5000");
+});
